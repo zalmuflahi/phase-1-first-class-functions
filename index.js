293a1c5d1b3d918a0b657ef23cc1d8895b86131f
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-13 13:46:50 -0400

    commit tree 5dbd744f165525c57db78cfe4181d739b02524b1

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(function_name) {
+    function_name()
+}
+
+function returnsANamedFunction() {
+   return function function_name(){}
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){}
+}
+
